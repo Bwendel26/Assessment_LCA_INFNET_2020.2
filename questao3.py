f9953fe546583d374e2a9113e8cd4bb67aec85d1
brunoFernandes <bwendel26999@gmail.com>
2020-09-24 14:59:56 -0300

    Adicionando questao3 ja funcional, necessita de algumas correcoes.

--- questao3.py
+++ questao3.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # "Você já deve ter ouvido algum especialista dizer que as pessoas precisam dedicar, 
 # no máximo, 30% da sua renda mensal à moradia, 20% à educação e 15% ao transporte. 
 # Esses valores não devem ser totalmente desprezados, mas não podem funcionar como 
@@ -37,32 +39,50 @@
 
 
 def saudeFinanceira(rendaMensal, gastosMoradia, gastosEducacao, gastosTransporte):
-    
+    #Variaveis 
     totalGastos = gastosMoradia + gastosEducacao + gastosEducacao
     percentualTotalGastos = totalGastos * 100 / rendaMensal
     percentualMoradia = gastosMoradia * 100 / rendaMensal
     percentualEducacao = gastosEducacao * 100 / rendaMensal
     percentualTransporte = gastosTransporte * 100 / rendaMensal
+    resultadoPadraoString = "Seus gastos com {} comprometem {:.1f}% na sua renda total. O máximo recomendado é de {}%"
+    resultadoPadraoString.replace("\n", " ") #impedindo quebra de linha.
+    resultadoVariavelString = ""
+    
+        #listas de controle
     controleResultados = []
+    nomeDoGasto = ["moradia", "educação", "transporte"]
+    maxPercentualPorGasto = [30, 20, 15]
+    gastoMaxRecomendado = [(rendaMensal / 100) * maxPercentualPorGasto[0], (rendaMensal / 100) * maxPercentualPorGasto[1], (rendaMensal / 100) * maxPercentualPorGasto[2]]
+    listaPercentualPorGasto = [percentualMoradia, percentualEducacao, percentualTransporte]
 
-    if percentualTotalGastos <= 65:
-        stringResultado = "Seus gastos estão dentro da margem recomendada."
-        return stringResultado
+    if percentualMoradia < 30 and percentualEducacao < 20 and percentualTransporte < 15:
+        resultadoVariavelString = "Seus gastos estão dentro da margem recomendada."
     else: 
         if percentualMoradia > 30:
-            controleResultados[0] = 1
+            controleResultados.append(1)
         else:
-            controleResultados[0] = 0
+            controleResultados.append(0)
         if percentualEducacao > 20:
-            controleResultados[1] = 1
+            controleResultados.append(1)
         else: 
-            controleResultados[1] = 0
+            controleResultados.append(0)
         if percentualTransporte > 15:
-            controleResultados[2] = 1
+            controleResultados.append(1)
         else:
-            controleResultados[2] = 0
+            controleResultados.append(0)
 
+        print("Diagnóstico:")
+        for i in range(len(controleResultados)):
+            if controleResultados[i] == 1:
+                resultadoVariavelString = "Portanto, idealmente, o máximo de sua renda comprometida com {} deveria ser de R$ {}."
+                print(resultadoPadraoString.format(nomeDoGasto[i], listaPercentualPorGasto[i], maxPercentualPorGasto[i]))
+                print(resultadoVariavelString.format(nomeDoGasto[i], gastoMaxRecomendado[i]))
 
+            elif controleResultados[i] == 0:
+                resultadoVariavelString = "Seus gastos estão dentro da margem recomendada."
+                print(resultadoPadraoString.format(nomeDoGasto[i], listaPercentualPorGasto[i], maxPercentualPorGasto[i]))
+                print(resultadoVariavelString)
 
 #Main
 rendaMensal = float(input("Renda mensal total: R$ "))
